33a11b99f93cc9dee924dccdbb62fd61dff364da
Cel1s <hungiemunin@gmail.com>
2021-07-27 19:42:21 -0300

    movimento inicial e final do quadro cobra

--- script.js
+++ script.js
@@ -25,9 +25,26 @@ function criarCobrinha()
         context.fillRect(snake[i].x, snake[i].y, box, box);
     }
 }
+//pega o keydown (evento de clique) e vai chamar a função update
+document.addEventListener("keydown", update);
+//da o comando onde o addEventListener que chama a update e a mesma passa como argumento os eventos abaixo
+function update (event)
+{
+    if(event.keyCode == 37 && direction != "right") direction = "left";
+    if(event.keyCode == 38 && direction != "down") direction = "up";
+    if(event.keyCode == 39 && direction != "left") direction = "right";
+    if(event.keyCode == 40 && direction != "up") direction = "down";
+}
 
 function iniciarJogo()
 {
+//quando a cobra chegar no zero, ela recebe 15 pra ja iniciar no outro lado 
+    if(snake[0].x > 15 * box && direction == "right") snake[0].x = 0;
+    if(snake[0].x < 0 * box && direction == "left") snake[0].x = 16 * box;
+    if(snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
+    if(snake[0].y < 0 * box && direction == "up") snake[0].y = 16 * box;
+
+
     criarBG();
     criarCobrinha();
     //criando posições x e y para setar os movimentos dando um inicio de partida
