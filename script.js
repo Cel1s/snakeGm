0ebdadbf3535d41ef984de3c006f733f7169a14c
Cel1s <hungiemunin@gmail.com>
2021-07-28 17:29:31 -0300

    this is the end, beautiful friend

--- script.js
+++ script.js
@@ -9,6 +9,12 @@ snake[0] =
 }
 
 let direction = "right";
+//O math floor retira o ponto flutuante do numero gerado pelo math radom
+let food =
+{
+    x: Math.floor(Math.random() * 15 + 1) * box,
+    y: Math.floor(Math.random() * 15 + 1) * box,
+}
 
 //função que inicia o canvas
 function criarBG()
@@ -25,6 +31,14 @@ function criarCobrinha()
         context.fillRect(snake[i].x, snake[i].y, box, box);
     }
 }
+
+//função comida cobrinha
+function drawFood()
+{
+    context.fillStyle = "darkred";
+    context.fillRect(food.x, food.y, box, box);    
+}
+
 //pega o keydown (evento de clique) e vai chamar a função update
 document.addEventListener("keydown", update);
 //da o comando onde o addEventListener que chama a update e a mesma passa como argumento os eventos abaixo
@@ -44,9 +58,18 @@ function iniciarJogo()
     if(snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
     if(snake[0].y < 0 * box && direction == "up") snake[0].y = 16 * box;
 
+    for(i = 1; i < snake.length; i++)
+    {
+        if(snake[0].x == snake[i].x && snake[0].y == snake[i].y)
+        {
+            clearInterval(jogo);
+            alert('game over!')
+        }
+    }
 
     criarBG();
     criarCobrinha();
+    drawFood();
     //criando posições x e y para setar os movimentos dando um inicio de partida
     let snakeX = snake[0].x;
     let snakeY = snake[0].y;
@@ -55,8 +78,18 @@ function iniciarJogo()
     if(direction == "left") snakeX -= box;
     if(direction == "up") snakeY -= box;
     if(direction == "down") snakeY += box;
+
+    if(snakeX != food.x || snakeY != food.y)
+    {
+        snake.pop();
+    }//a comida recebe posições aleatorias tanto em x quanto em y
+    else
+    {
+        food.x = Math.floor(Math.random() * 15 + 1) * box;
+        food.y = Math.floor(Math.random() * 15 + 1) * box;
+    }
     // retira o ultimo elemento do array
-    snake.pop();
+    
     //criando a "cara" da cobra kkk função sempre acrescenta um espaço a frente
     let newHead = 
     {
